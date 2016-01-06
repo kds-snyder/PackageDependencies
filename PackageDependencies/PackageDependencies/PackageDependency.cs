1238c7449372b0fa77d22a95676252d9322fcf64
kds-snyder <kdsnyderdev@gmail.com>
2016-01-06 14:51:34 -0800

    Refactor to move class PackageDependency to separate project

--- PackageDependencies/PackageDependencies/PackageDependency.cs
+++ PackageDependencies/PackageDependencies/PackageDependency.cs
@@ -2,13 +2,13 @@
 
 namespace PackageDependencies.Test
 {
-    internal class PackageDependency
+    public class PackageDependency
     {
         public PackageDependency()
         {
         }
 
-        internal string GetInstallListFromDependencies(string[] v)
+        public string GetInstallListFromDependencies(string[] v)
         {
             int indexPackageDependencyDelimiter = v[0].IndexOf(":");
             return v[0].Substring(0, indexPackageDependencyDelimiter);
