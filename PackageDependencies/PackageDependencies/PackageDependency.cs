eaf1c74a893c676d4cb1285a13ce66e2e3a3b4e6
kds-snyder <kdsnyderdev@gmail.com>
2016-01-07 10:57:15 -0800

    Refactor to move parse method to separate class

--- PackageDependencies/PackageDependencies/PackageDependency.cs
+++ PackageDependencies/PackageDependencies/PackageDependency.cs
@@ -1,17 +1,14 @@
 ﻿namespace PackageDependencies
 {
     public class PackageDependency
-    {
-        private const char PACKAGE_DEPENDENCY_DELIMITER = ':';
-        private const int PACKAGE_DEPENDENCY_OFFSET = 2;
-
+    {    
         public PackageDependency()
         {
         }
 
         public string GetInstallListFromDependencies(string[] packageDependencyPairs)
         {         
-            var parsedPackageDependencyPair = parsePackageDependencPair(packageDependencyPairs[0]);
+            var parsedPackageDependencyPair = Parse.Instance.ParsePackageDependencPair(packageDependencyPairs[0]);
 
             if (parsedPackageDependencyPair.NeededPackage != null)
             {
@@ -22,21 +19,6 @@
                 return parsedPackageDependencyPair.MainPackage;
             }                  
         }
-
-        private ParsedPackageDependencyPair parsePackageDependencPair(string packageDependencyPair)
-        {
-            var parsedPackageDependencyPair = new ParsedPackageDependencyPair();
-
-            int indexPackageDependencyDelimiter = packageDependencyPair.IndexOf(PACKAGE_DEPENDENCY_DELIMITER);
-
-            parsedPackageDependencyPair.MainPackage = packageDependencyPair.Substring(0, indexPackageDependencyDelimiter);
-            if (indexPackageDependencyDelimiter + PACKAGE_DEPENDENCY_OFFSET < packageDependencyPair.Length)
-            {
-                parsedPackageDependencyPair.NeededPackage =
-                    packageDependencyPair.Substring(indexPackageDependencyDelimiter + PACKAGE_DEPENDENCY_OFFSET);
-            }
-
-            return parsedPackageDependencyPair;
-        }
+       
     }
 }
---
+++ PackageDependencies/PackageDependencies/Parse.cs
@@ -0,0 +1,27 @@
+﻿namespace PackageDependencies
+{
+    public class Parse
+    {
+        private const char PACKAGE_DEPENDENCY_DELIMITER = ':';
+        private const int PACKAGE_DEPENDENCY_OFFSET = 2;
+
+        public static Parse Instance => new Parse();
+
+        public ParsedPackageDependencyPair ParsePackageDependencPair(string packageDependencyPair)
+        {
+            var parsedPackageDependencyPair = new ParsedPackageDependencyPair();
+
+            int indexPackageDependencyDelimiter = packageDependencyPair.IndexOf(PACKAGE_DEPENDENCY_DELIMITER);
+
+            parsedPackageDependencyPair.MainPackage = packageDependencyPair.Substring(0, indexPackageDependencyDelimiter);
+
+            if (indexPackageDependencyDelimiter + PACKAGE_DEPENDENCY_OFFSET < packageDependencyPair.Length)
+            {
+                parsedPackageDependencyPair.NeededPackage =
+                    packageDependencyPair.Substring(indexPackageDependencyDelimiter + PACKAGE_DEPENDENCY_OFFSET);
+            }
+
+            return parsedPackageDependencyPair;
+        }
+    }
+}
